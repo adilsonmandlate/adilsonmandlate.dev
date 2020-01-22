60bd1af33734efc9f7cc06a180ac5aeffe5e039d
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-01-22 17:23:10 +0200

    Change Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,12 +31,12 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Adilson Mandlate`,
-        short_name: `adilson`,
+        short_name: `Adilson`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1a1a1a`,
+        theme_color: `#96979c`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
