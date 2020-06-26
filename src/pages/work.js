b81fd80634b13dff93f4a7bc3e7da5f103289b15
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-06-26 17:55:33 +0200

    feat: Create Work showcase component

---
+++ src/pages/work.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+import SEO from "../components/seo";
+
+const WorkPage = () => (
+  <Layout>
+    <SEO title="Work page" />
+    <div>Lista de trabalho heeee</div>
+  </Layout>
+);
+
+export default WorkPage;
