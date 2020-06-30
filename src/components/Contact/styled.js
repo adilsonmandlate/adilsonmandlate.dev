318af705d41d8163944cbe01d7808cc9b7e18f2f
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-06-30 18:50:32 +0200

    style: Update contact styling

--- src/components/Contact/styled.js
+++ src/components/Contact/styled.js
@@ -1,13 +1,39 @@
 import styled from "styled-components";
-import { Mail } from "styled-icons/typicons/Mail";
 
-export const MailIcon = styled(Mail)`
+export const ContactContainer = styled.div`
+  flex-grow: 1;
+  padding: 0 0 10px 0;
+  font-size: calc(10px + (50 - 10) * (100vw - 400px) / (6400 - 400));
+`;
+
+export const Label = styled.p`
+  font-weight: 600;
+  padding: 20px 0;
+  text-transform: uppercase;
+`;
+
+export const Email = styled.a`
   color: #fff;
-  position: fixed;
-  bottom: 2vw;
-  transition: all 0.2s ease-in-out;
+  position: relative;
+  text-decoration: none;
+
+  &:before {
+    content: "";
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 100%;
+    width: 100%;
+    height: 2px;
+    transform: scaleX(0);
+    background-color: #00bcd4;
+    transition: transform 0.3s;
+  }
 
   &:hover {
-    color: #e74c3c;
+    &:before {
+      transform: scaleX(1);
+    }
   }
 `;
