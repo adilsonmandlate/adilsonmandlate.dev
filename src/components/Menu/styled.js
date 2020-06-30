b2c60661cecd56de7063e3c7ef72d3930b8930b6
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-06-30 18:46:04 +0200

    style: Organizing vertical menu to support more than one link

--- src/components/Menu/styled.js
+++ src/components/Menu/styled.js
@@ -2,39 +2,40 @@ import styled from "styled-components";
 
 export const Container = styled.ul`
   display: flex;
-  flex-direction: row-reverse;
-  width: 100vh;
-  padding: 0 70px;
-  transform: rotate(-90deg);
+  flex-direction: column-reverse;
+  justify-content: center;
+  padding: 20px 0;
+  font-size: calc(14px + (50 - 14) * (100vw - 400px) / (6400 - 400));
 
   li {
-    margin: 10px;
+    padding: 30px;
+    align-self: center;
     text-transform: uppercase;
+    color: #fff;
+    transform: rotate(-90deg);
   }
 
   a {
     position: relative;
     font-weight: 500;
-    font-size: calc(14px + (80 - 14) * (100vw - 400px) / (6400 - 400));
     color: #fff;
     text-decoration: none;
 
-    &:before {
+    &:after {
       content: "";
       position: absolute;
       left: 0;
       right: 0;
       bottom: 0;
       top: 100%;
-      width: 100%;
-      height: 4px;
+      height: 2px;
       transform: scaleX(0);
       background-color: #00bcd4;
       transition: transform 0.3s;
     }
 
     &:hover {
-      &:before {
+      &:after {
         transform: scaleX(1);
       }
     }
