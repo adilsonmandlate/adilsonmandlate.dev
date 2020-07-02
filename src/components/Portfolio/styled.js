4a27593bbeef7084299a37869f366f79b937e81f
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-07-02 19:02:00 +0200

    feat: Show project details

--- src/components/Portfolio/styled.js
+++ src/components/Portfolio/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Img from "gatsby-image";
 
 export const Container = styled.div`
   padding: 3.125em 0;
@@ -19,4 +20,27 @@ export const TitleSeparator = styled.hr`
     rgba(0, 188, 212, 1) 40%,
     rgba(31, 31, 31, 1) 115%
   );
+`;
+
+export const ProjectDetails = styled.li`
+  margin: 0 0 80px;
+  display: grid;
+  gap: 50px;
+  grid-template-columns: 320px 1fr;
+`;
+
+export const ProjectImage = styled(Img)`
+  max-width: 300px;
+`;
+
+export const ProjectName = styled.h3`
+  font-size: 1.4rem;
+  font-weight: 600;
+`;
+
+export const ProjectDescription = styled.p`
+  margin: 20px 0;
+  font-size: 1.2rem;
+  line-height: 2rem;
+  text-align: justify;
 `;
