8974a4a4911bf7a0681913a71072217232e6ae40
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-08-10 18:37:19 +0200

    chore: remove unused components

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import Layout from "../components/Layout";
 import SEO from "../components/seo";
 import Home from "../components/Home/Home";
-import Contact from "../components/Contact";
 
 const IndexPage = () => (
   <Layout>
