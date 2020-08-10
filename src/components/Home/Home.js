95a999ee455f16bf323911919b7dc89bc04db2b3
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-08-10 18:33:29 +0200

    style: add image to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,41 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import Img from "gatsby-image";
 import styles from "./Home.module.css";
 
-const WelcomeInfo = () => {
+const Home = () => {
+  const imageQuery = graphql`
+    query GetProfileImage {
+      file(relativePath: { eq: "profile-bw.png" }) {
+        childImageSharp {
+          fixed(width: 140) {
+            ...GatsbyImageSharpFixed_tracedSVG
+          }
+        }
+      }
+    }
+  `;
+
+  const {
+    file: { childImageSharp },
+  } = useStaticQuery(imageQuery);
+
   return (
     <section className={styles.container}>
       <div>
-        <h1 className={styles.greetings}>
-          Hi<span className={styles.comma}>,</span>
+        <h1 className={styles.name}>
+          Adilson Mandlate
+          <Img className={styles.nameImg} fixed={childImageSharp.fixed} />
         </h1>
         <h1 className={styles.greetings}>
-          I'm Adilson Mandlate
+          Front End Developer
           <span className={styles.dot} />
         </h1>
-        <p className={styles.description}>
-          A front-end developer based in Mozambique
-        </p>
+
+        <p className={styles.description}>(Not) Your avarage developer</p>
       </div>
     </section>
   );
 };
 
-export default WelcomeInfo;
+export default Home;
