4b959975b737ddcc70487cfb6c81de556fd7d992
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-06-01 15:28:42 +0200

    chore: configure tailwind and postcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "media",
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        app: "1fr 50px",
+      },
+      minHeight: {
+        app: "calc(100vh - 50px)",
+      },
+      textColor: {
+        title: "#2c3e50",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
