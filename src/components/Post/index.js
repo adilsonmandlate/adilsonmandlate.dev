a1e4fe02cf94509438a42ae4dff8ae8db2de5b1d
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-06-16 11:16:12 +0200

    chore: add mdx components file

---
+++ src/components/Post/index.js
@@ -0,0 +1,11 @@
+// In here, we'll list all components to be used in
+// mdx files. Since we cannot import directly there,
+// we'll have to gather and inject from an external component (here)
+// and pass it through MDXRemote component (you'll find it in /blog/[slug].js)
+
+/* i.e.
+const Components = {
+  Header, // from 'import Header from "Header"'
+};
+
+*/
