ce1f539c967d6b251b9b81cb392d1c604621ae75
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-06-16 11:18:49 +0200

    feat: create blog page

---
+++ src/pages/blog/index.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const BlogIndex = () => (
+  <>
+    <h1>Blog</h1>
+    <p>
+      Welcome to my blog, or whaterever this is. You should be able to see this
+      with no problems. Facing any issue? Talk to me
+    </p>
+  </>
+);
+
+export default BlogIndex;
