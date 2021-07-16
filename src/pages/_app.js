817c7a4d91016cee87b8d20e874936eefdf7a41b
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-07-16 17:49:43 +0200

    feat: update app docuemnt

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,18 @@
 import "../styles/global.css";
 import { Footer } from "../components/Footer";
+import { Navbar } from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="grid grid-rows-app">
-      <div className="min-h-app flex justify-center self-center">
-        <main className="max-w-5xl p-12 h-full m-auto">
+      <Navbar />
+      <div className="min-h-app pt-20 flex justify-center self-center">
+        <main className="max-w-3xl p-12 h-full m-auto">
           <Component {...pageProps} />
         </main>
       </div>
 
-      <Footer className="self-center mb-8 md:mb-2 text-center" />
+      <Footer className="max-w-full self-center mb-8 md:mb-2 text-center" />
     </div>
   );
 }
