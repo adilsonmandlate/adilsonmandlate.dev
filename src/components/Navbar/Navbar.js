33ebc98a528d27e1be2aed7f81fbd10f518ce724
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-07-18 14:25:21 +0200

    feat: add image to home page link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-row items-center fixed top-0 right-0 left-0 justify-between max-w-3xl w-full mx-auto px-12 py-6 md:py-12 bg-white dark:bg-gray-900">
+    <nav className="custom-nav flex flex-row items-center fixed top-0 right-0 left-0 justify-between max-w-6xl w-full mx-auto px-6 md:px-12 py-4 bg-white dark:bg-gray-900">
       <Link href="/">
-        <a className="text-gray-800 dark:text-gray-50 no-underline">Home</a>
+        <a className="text-gray-800 dark:text-gray-50 no-underline">
+          <Image
+            src="/icon-512.png"
+            width={38}
+            height={38}
+            alt="Adilson Mandlate"
+          />
+        </a>
       </Link>
 
       <Link href="/blog/test-post">
