c552b3766e5369d3e8f0e7fb92ed28ca0a77f009
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-07-19 21:11:24 +0200

    feat: remove glass effect from navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav className="custom-nav flex flex-row items-center fixed top-0 right-0 left-0 justify-between max-w-6xl w-full mx-auto px-6 md:px-12 py-4 bg-white dark:bg-gray-900">
+    <nav className="flex flex-row items-center fixed top-0 right-0 left-0 justify-between max-w-4xl w-full mx-auto px-6 md:px-12 py-4 bg-white dark:bg-gray-900">
       <Link href="/">
         <a className="text-gray-800 dark:text-gray-50 no-underline">
           <Image
