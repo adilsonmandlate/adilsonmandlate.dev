6214f2ded1fb5e075385e2b49dd1b1c8f9631f98
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-07-19 21:13:15 +0200

    chore: remove unnecessary div's

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,14 +4,11 @@ import { Navbar } from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="grid grid-rows-app">
+    <div className="grid grid-rows-app min-h-screen justify-items-center pt-24">
       <Navbar />
-      <div className="min-h-app pt-24 flex justify-center self-center">
-        <main className="max-w-3xl w-full px-6 md:px-12 h-full my-auto">
-          <Component {...pageProps} />
-        </main>
-      </div>
-
+      <main className="max-w-3xl w-full px-6 md:px-12 h-full my-auto">
+        <Component {...pageProps} />
+      </main>
       <Footer className="max-w-full self-center mb-8 md:mb-2 text-center" />
     </div>
   );
