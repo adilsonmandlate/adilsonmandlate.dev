42dd8704a8d0f73a57fdcc875d98601b1a4d29f4
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-07-19 18:34:30 +0200

    feat: list all posts

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,12 +1,34 @@
-import Link from "next/link";
+import matter from "gray-matter";
+import Head from "next/head";
+import { Fragment } from "react";
+import { BlogPost } from "../../components/BlogPost";
+import { getAllPostsData } from "../../lib/api";
 
-const BlogIndex = () => (
-  <>
-    <h1 className="text-6xl md:text-7xl font-bold tracking-tight mb-8 text-black dark:text-white">
-      Blog
-    </h1>
-    <p>Once upon a time, I wrote...</p>
-  </>
+const BlogIndex = ({ posts }) => (
+  <Fragment>
+    <Head>
+      <title>Blog - Adilson Mandlate | Front end developer</title>
+    </Head>
+
+    <header className="mb-10">
+      <h1 className="text-4xl md:text-5xl font-bold tracking-tight mb-4 text-black dark:text-white">
+        Blog
+      </h1>
+
+      <p>Once upon a time, I wrote about...</p>
+    </header>
+
+    {posts.map((post) => {
+      return (
+        <BlogPost
+          key={post.title}
+          title={post.title}
+          description={post.description}
+          slug={post.slug}
+        />
+      );
+    })}
+  </Fragment>
 );
 
 export async function getStaticProps() {
