4ff45ec1aaf8dbeb5767c6d90e3bd6262db21f97
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-07-25 20:43:36 +0200

    style: add an active state for blog nav link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
   return (
-    <nav className="flex flex-row items-center fixed top-0 right-0 left-0 justify-between max-w-4xl w-full mx-auto px-6 md:px-12 py-4 bg-white dark:bg-gray-900">
+    <nav className="flex flex-row items-center fixed top-0 right-0 left-0 justify-between max-w-7xl w-full mx-auto px-6 md:px-12 py-4 bg-white dark:bg-gray-900">
       <Link href="/">
         <a className="text-gray-800 dark:text-gray-50 no-underline">
           <Image
@@ -16,7 +18,16 @@ const Navbar = () => {
       </Link>
 
       <Link href="/blog/">
-        <a className="text-gray-800 dark:text-gray-50 no-underline">Blog</a>
+        <a
+          className={`
+            ${
+              router.pathname === "/blog"
+                ? "underline font-bold"
+                : "no-underline"
+            } text-gray-800 dark:text-gray-50`}
+        >
+          Blog
+        </a>
       </Link>
     </nav>
   );
