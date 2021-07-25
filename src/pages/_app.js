271e3a56aca67adb35fb576e36eec054d6ea8746
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-07-25 20:46:55 +0200

    style: center footer content

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,10 +6,10 @@ function MyApp({ Component, pageProps }) {
   return (
     <div className="grid grid-rows-app min-h-screen justify-items-center pt-24">
       <Navbar />
-      <main className="max-w-3xl w-full px-6 md:px-12 h-full my-auto">
+      <main className="max-w-5xl w-full px-6 md:px-12 h-full my-auto">
         <Component {...pageProps} />
       </main>
-      <Footer className="max-w-full self-center mb-8 md:mb-2 text-center" />
+      <Footer className="max-w-full w-full h-full flex justify-center items-center" />
     </div>
   );
 }
