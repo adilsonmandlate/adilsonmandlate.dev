1d19c7a0ef34f889775b7af4abaf2b94b6325885
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-07-25 20:42:39 +0200

    feat: add published date on post lists

--- src/components/BlogPost/Post.js
+++ src/components/BlogPost/Post.js
@@ -1,17 +1,26 @@
 import Link from "next/link";
+import { format, parseISO } from "date-fns";
 
-const Post = ({ title, description, slug }) => {
+const Post = ({ title, description, slug, date }) => {
   return (
-    <Link href={`/blog/${slug}`}>
-      <a className="w-full no-underline">
-        <div className="mb-9">
-          <h3 className="text-lg font-medium md:text-xl mb-2 text-gray-800 dark:text-gray-50">
-            {title}
-          </h3>
-          <p className="text-gray-600 dark:text-gray-200">{description}</p>
-        </div>
-      </a>
-    </Link>
+    <li className="pb-16">
+      <Link href={`/blog/${slug}`}>
+        <a className="w-full no-underline">
+          <div>
+            <h3 className="text-xl font-medium md:text-3xl mb-1 text-gray-800 dark:text-gray-50">
+              {title}
+            </h3>
+            <p className="mb-3 text-gray-600 dark:text-gray-200">
+              {description}
+            </p>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Post published on{" "}
+              <time>{format(parseISO(date), "MMMM dd, yyy")}</time>
+            </span>
+          </div>
+        </a>
+      </Link>
+    </li>
   );
 };
 
