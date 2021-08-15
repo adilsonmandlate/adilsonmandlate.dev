2367c8b58bc4ed7aaccc7122147b4ac998c148f5
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-08-15 16:08:09 +0200

    style: add margin-y

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 const Navbar = () => {
   const router = useRouter();
   return (
-    <nav className="flex flex-row items-center fixed top-0 right-0 left-0 justify-between max-w-7xl w-full mx-auto px-6 md:px-12 py-4 bg-white dark:bg-gray-900">
+    <nav className="flex flex-row items-center fixed top-0 right-0 left-0 justify-between max-w-7xl w-full my-12 mx-auto px-6 md:px-12 py-4 bg-white dark:bg-gray-900">
       <Link href="/">
         <a className="text-gray-800 dark:text-gray-50 no-underline">
           <Image
