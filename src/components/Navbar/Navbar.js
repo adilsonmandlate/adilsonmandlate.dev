9b374a880e9823ac6d648cbbb4081a7a1983cd18
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-08-15 16:09:47 +0200

    chore: remove userouter definition

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
-import { useRouter } from "next/router";
 
 const Navbar = () => {
-  const router = useRouter();
   return (
     <nav className="flex flex-row items-center fixed top-0 right-0 left-0 justify-between max-w-7xl w-full my-12 mx-auto px-6 md:px-12 py-4 bg-white dark:bg-gray-900">
       <Link href="/">
