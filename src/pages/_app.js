9ea193ab105d0584e819c187a5d1866dcb2b4004
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-08-17 18:56:46 +0200

    style: change padding on main page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,7 @@ import { Navbar } from "../components/Navbar";
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
-      <div className="grid grid-rows-app min-h-screen justify-items-center pt-48">
+      <div className="grid grid-rows-app min-h-screen justify-items-center pt-40">
         <Navbar />
         <main className="max-w-5xl w-full px-6 md:px-12 h-full my-auto">
           <Component {...pageProps} />
