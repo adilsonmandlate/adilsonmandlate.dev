b4eec0ade3d10ac5fe6de0edb6c5fe5a6a776865
Adilson Mandlate <adilsonmandlate@gmail.com>
2023-03-22 18:45:37 +0200

    style: centre the social link icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-    <footer className="w-full h-full grid md:grid-cols-2 max-w-7xl items-center pt-2 md:px-12 md:pt-0 border-t border-gray-700 dark:border-gray-800 border-opacity-5">
-      <div className="flex justify-self-center md:justify-self-start mt-4 md:mt-0">
+    <footer className="w-full h-full grid md:grid-cols-2 max-w-7xl items-center md:px-12 border-t border-gray-700 dark:border-gray-800 border-opacity-5">
+      <div className="flex justify-self-center md:justify-self-start md:mt-0">
         <a
           href="https://github.com/adilsonmandlate"
           rel="nofollow noopener noreferrer"
