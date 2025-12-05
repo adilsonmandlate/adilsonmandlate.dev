3b544442ddd2612a0558364e113112a323487194
Adilson Mandlate <adilson.mandlate@interactive.co.mz>
2025-12-05 16:17:20 +0200

    feat: update menu desing

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <nav className="fixed z-10 top-0 right-0 left-0 w-full max-w-xs mx-auto flex justify-center py-5 px-4">
       <div
-        className="flex flex-row items-center justify-between gap-4 rounded-full px-4 shadow-2xl  w-full"
+        className="flex flex-row items-center justify-between gap-4 rounded-full px-[6px] shadow-2xl  w-full"
         style={{
           background:
             "linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.05))",
@@ -23,14 +23,14 @@ const Navbar = () => {
       >
         <Link
           href="/"
-          className={`no-underline p-2 rounded-full transition-all duration-300 hover:bg-white/10 ${
+          className={`no-underline p-2 rounded-full my-1 transition-all duration-300 hover:bg-white/10 ${
             activeRoute === "/" && "bg-white/10"
           }`}
         >
           <Image
             src="/icon-512.png"
-            width={32}
-            height={32}
+            width={24}
+            height={24}
             alt="Adilson Mandlate"
             className="rounded-full"
           />
@@ -39,7 +39,7 @@ const Navbar = () => {
         <Link
           href="/work"
           className={`text-sm font-medium px-4 py-2 rounded-full transition-all duration-300 hover:text-white/80 no-underline decoration-1 underline-offset-4 ${
-            activeRoute === "/work" && "font-bold no-underline"
+            activeRoute === "/work" && "font-bold no-underline bg-white/10"
           }`}
         >
           Work
